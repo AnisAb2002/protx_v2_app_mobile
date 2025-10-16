cc65763adbe77ee48068dc1119e8dff80c4b80a6
AnisAb2002 <anisgjr@gmail.com>
2025-10-16 13:45:27 +0200

    -J'ai ajouté la fonction du bouton Accéder pour aller de MainActivity vers Accuil activity - J'ai ajouté des détails sur catalogue produit

--- app/src/main/java/fr/devmobile/protx_v2/MainActivity.kt
+++ app/src/main/java/fr/devmobile/protx_v2/MainActivity.kt
@@ -1,20 +1,26 @@
 package fr.devmobile.protx_v2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import fr.devmobile.protx_v2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAcceder.setOnClickListener {
+            val intent = Intent(this, Accueil::class.java)
+            startActivity(intent)
+            finish()
         }
+
     }
 }
